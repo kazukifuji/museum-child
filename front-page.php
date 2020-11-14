cc129451b952a1534a3daa236b3606750d514fc2
fkazu <ketuago.kazu@outlook.jp>
2020-11-14 20:53:41 +0900

    Create a 'home-page-contents' block element and its rough components
    'home-page-contents'ブロック要素と、その大まかなコンポーネントを作成。

--- front-page.php
+++ front-page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+  <article class="home-page-contents">
 
+    <section class="home-page-contents__profile"></section>
+
+    <section class="home-page-contents__works"></section>
+
+    <section class="home-page-contents__blogs"></section>
+
+    <div class="home-page-contents__social-media-accounts"></div>
+
+  </article><!--.home-page-contents-->
 
 <?php get_footer(); ?>
