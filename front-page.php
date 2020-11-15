4529b74bb45a0701a77d5ab549249ad31229c516
fkazu <ketuago.kazu@outlook.jp>
2020-11-15 22:24:10 +0900

    Add profile section to homepage contents
    ホームページコンテンツにプロフィールセクションを追加。

--- front-page.php
+++ front-page.php
@@ -2,7 +2,29 @@
 
   <article class="home-page-contents">
 
-    <section class="home-page-contents__profile"></section>
+    <section class="home-page-contents__profile">
+      <h2 class="home-page-contents__heading -center">
+        <span class="home-page-contents__heading-text">Profile</span>
+      </h2>
+      
+      <div class="wrapper">
+        <figure class="home-page-contents__profile-avatar">
+          <?php echo get_avatar( 1, 175 ); ?>
+        </figure>
+
+        <p class="home-page-contents__profile-name">
+          <?php the_author_meta( 'display_name', 1 ); ?>
+        </p>
+
+        <?php
+        $description = esc_html( get_the_author_meta( 'description', 1 ) );
+        if ( $desc !== '' ) : ?>
+          <p class="home-page-contents__profile-description">
+            <?php echo $description; ?>
+          </p>
+        <?php endif; ?>
+      </div><!--.wrapper-->
+    </section>
 
     <section class="home-page-contents__works"></section>
 
