3d36395dd930faeafb98f7484cf7b6bbc26f9a90
fkazu <ketuago.kazu@outlook.jp>
2020-11-17 20:00:00 +0900

    Add works section to homepage contents
    ホームページコンテンツに作品セクションを追加。

--- front-page.php
+++ front-page.php
@@ -26,7 +26,32 @@
       </div><!--.wrapper-->
     </section>
 
-    <section class="home-page-contents__works"></section>
+    <section class="home-page-contents__works">
+      <h2 class="home-page-contents__heading -left">
+        <span class="home-page-contents__heading-text">Works</span>
+      </h2>
+
+      <ul class="home-page-contents__works-list">
+        <?php
+        $post_id_arr = [ 1177, 1016, 1011, 555, 1163, 2135 ];
+
+        foreach ( $post_id_arr as $post_id ) :
+          if ( has_post_thumbnail( $post_id ) ) : ?>
+            <li class="home-page-contents__works-list-item">
+              <a class="home-page-contents__works-list-item-link" href="<?php echo esc_url( get_permalink( $post_id ) ); ?>">
+                <figure class="home-page-contents__works-list-item-featured-media">
+                  <?php echo get_the_post_thumbnail( $post_id, 'post-thumbnail', [ 'data-object-fit' => 'cover' ] ); ?>
+                </figure>
+              </a>
+            </li>
+          <?php endif;
+        endforeach ?>
+      </ul>
+
+      <a class="home-page-contents__works-more-button button-1 -large" href="<?php echo esc_url( get_post_type_archive_link( 'works' ) ); ?>">
+        MORE WORKS
+      </a>
+    </section>
 
     <section class="home-page-contents__blogs"></section>
 
