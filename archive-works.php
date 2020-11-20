e0352ae71256607069c36381bceb0bed1954b2dc
fkazu <ketuago.kazu@outlook.jp>
2020-11-20 19:49:48 +0900

    Create a custom post archive page of works and blogs
    作品とブログのカスタム投稿アーカイブページを作成。

---
+++ archive-works.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+  <h1 class="main__heading-1" data-subtitle="- works -">作品</h1>
+
+  <section>
+    <div class="wrapper">
+      <h2 class="main__heading-2">作品一覧</h2>
+    </div><!--.wrapper-->
+    <?php get_template_part('template_parts/post-list'); ?>
+  </section>
+
+<?php get_footer(); ?>
