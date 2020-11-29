60db27dd6b1376f49c94f5ac33e6822917985644
fkazu <ketuago.kazu@outlook.jp>
2020-11-29 12:32:56 +0900

    Add retry rules for custom post types 'works' and 'blogs' #18
    カスタム投稿タイプの'works'と'blogs'のためのリトライルールを追加。

--- functions.php
+++ functions.php
@@ -88,6 +88,16 @@ function custom_post_type_link( $link, $post ) {
   }
 }
 
+//リトライルールを追加
+add_filter( 'rewrite_rules_array', 'add_rewrite_rules' );
+function add_rewrite_rules( $rules ) {
+  $new_rules = [
+    'works/([0-9]+)/?$' => 'index.php?post_type=works&p=$matches[1]',
+    'blogs/([0-9]+)/?$' => 'index.php?post_type=blogs&p=$matches[1]',
+  ];
+  return $new_rules + $rules;
+}
+
 //ホームページコンテンツのプロフィールセクションを表示するショートコード
 add_shortcode( 'profile_section', 'museum_child_profile_section' );
 function museum_child_profile_section( $atts ) {
